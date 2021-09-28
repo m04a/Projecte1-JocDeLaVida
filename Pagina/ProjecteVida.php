1ebe0318cc048ce16e26de93095b6a6a69764067
Moha2001B <molamedb@gmail.com>
2021-09-28 17:32:42 +0200

    Arreglat error

--- Pagina/ProjecteVida.php
+++ Pagina/ProjecteVida.php
@@ -5,13 +5,15 @@
 <h1>La meva pagina php</h1>
 
 <table>
-<?php 
-for ($x = 0; $x <= 10; $x++) {?>
+<?php
+$x = $_POST['GrandariaX'];
+$y = $_POST['GrandariaY'];
+for ($x = 0; $x <= GrandariaX; $x++) {?>
 	<tr class="files">
-	<?php for ($x = 0; $x <= 10; $x++) {?>
+	<?php for ($y = 0; $y <= GrandariaY; $y++) {?>
 		<td class="columnes">
 			<input type="checkbox">
 		</td>
-?>
+<?php}}?>
 </body>
 </html>
