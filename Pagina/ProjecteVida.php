91c0d9443cffa7769ff9c76007ba451a14867407
moha <mohamedbourarach@gmail.com>
2021-10-01 16:38:33 +0200

    Afegit el js i millores en la escala de les cel·lules

--- Pagina/ProjecteVida.php
+++ Pagina/ProjecteVida.php
@@ -2,6 +2,7 @@
 <html>
 <head>
   <link rel="stylesheet" href="style.css">
+  <script type="text/javascript" src="script.js"></script>
   <link href=' http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
 </head>
 <body>
@@ -12,21 +13,8 @@
 <?php
 $xGra = $_POST['GrandariaX'];
 $yGra = $_POST['GrandariaY'];
-<<<<<<< HEAD
-if ($xGra == null || $yGra ==null){
-echo "Les dades intrudides no són correctes";
-}
-else{
-for ($x = 1; $x <= $xGra; $x++) {?>
-        <tr class="files">
-        <?php for ($y = 1; $y <= $yGra; $y++) {?>
-                <td class="columnes">
-                        <input type="checkbox">
-                </td>
-}
-<?php }?>
-<?php }?>
-=======
+
+
 if ($xGra < 3 || $xGra == null || $yGra < 3 || $yGra == null || $yGra > 40 || $xGra > 20){
 echo "<p class='error'>Error: Si us plau, revisa les dades introduïdes. Es farà una redirecció en 3 segons...</p>";
 header('Refresh: 3; Projecte1.html');
@@ -40,8 +28,9 @@ else{
 		echo "</td>";
 }
 }
-}?>
->>>>>>> 58272b6f2dc27aefa1dd67af603ed321bdc908c2
+}
+echo '<button class="btn" onclick="jsFunction()">Jugar</button>';
+?>
 </table>
 </body>
 </html>
