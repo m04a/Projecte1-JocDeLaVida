25f48b8003bdc620fa9b427d496ebe97f769caf5
moha <mohamedbourarach@gmail.com>
2021-10-04 16:41:44 +0200

    Afegida una nova pagina per crear la taula final

---
+++ Pagina/taula.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+  <script type="text/javascript" src="script.js"></script>
+  <link href=' http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<?php 
+   $xGra = $_POST['xGra'];
+   $yGra = $_POST['yGra'];
+   $Array= $_POST['tauler'];
+?>
+<?php 
+echo "<table>";
+for ($x = 1; $x <= $xGra; $x++) {
+       echo  "<tr class='files'>";
+         for ($y = 1; $y <= $yGra; $y++) {
+              echo "<td class='columnes'>";
+			if (isset($Array[$x][$y])) {
+                        echo "<input type='checkbox' name='tauler[".$y."][".$x."]'  class='celules' checked>" ;
+                	}
+			else{
+		        echo "<input type='checkbox' name='tauler[".$y."][".$x."]'  class='celules'>" ;
+			}
+  			echo "</td>";
+}
+echo "</tr>";
+}
+echo "</table>";
+?>
+</body>
+</html>
