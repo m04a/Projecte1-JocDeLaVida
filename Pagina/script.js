d27e8d4b58dfed0196913b9c99fe78c0436181a0
Moha2001B <molamedb@gmail.com>
2021-10-06 11:41:44 +0200

    Millora js

--- Pagina/script.js
+++ Pagina/script.js
@@ -9,14 +9,14 @@ var tauler = new Array(num);
 var taulerProx = new Array(num);
 console.log(num);
     for (var i = 0; i < num; i++) {
-	tauler[i] = new Array(num2);
-	taulerProx[i] = new Array(num2);
+        tauler[i] = new Array(num2);
+        taulerProx[i] = new Array(num2);
 }
 
    for (var i = 0; i < num; i++) {
-		for (var j = 0; j < num2; j++) {
-		tauler[i][j] = 0;
-		taulerProx[i][j] = 0;
+                for (var j = 0; j < num2; j++) {
+                tauler[i][j] = 0;
+                taulerProx[i][j] = 0;
      }
 }
 for (var i = 0; i < num; i++) {
@@ -29,8 +29,31 @@ for (var i = 0; i < num; i++) {
         for (var j = 0; j < num2; j++) {
             aplicarNormes(i, j);
         }
+
+copiarIResetNexGen(num,num2);
+actualitzarTempsreal(num,num2);
     }
 console.log(tauler);
+}
+function copiarIResetNexGen(){
+ for (var i = 0; i < rows; i++) {
+          for (var j = 0; j < cols; j++) {
+              tauler[i][j] = taulerProx[i][j];
+              taulerProx[i][j] = 0;
+          }
+      }
+}
+function actualitzarTempsreal(num,num2){
+for (var i = 0; i < num; i++) {
+            for (var j = 0; j < num2; j++) {
+                var celula = document.getElementsByName(tauler + "[" + i + "]" + "[" + j + "]");
+                if (tauler[i][j] == 0) {
+                    celula.setAttribute("checked", "true");
+                } else {
+                    celula.setAttribute("checked", "false");
+                }
+            }
+        }
 }
 function aplicarNormes(num, num2) {
 //Ens farà el return de la funció que conta els veins
@@ -49,9 +72,9 @@ function aplicarNormes(num, num2) {
             }
         }
     }
-	
-	//Necessito el numero original de lineas i columnes per la següent condició
-	function contarVeins(num, num2) {
+
+        //Necessito el numero original de lineas i columnes per la següent condició
+        function contarVeins(num, num2) {
     var count = 0;
     if (num-1 >= 0) {
         if (tauler[num-1][num2] == 1) count++;
